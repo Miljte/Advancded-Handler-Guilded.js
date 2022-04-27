05ead55d5389985d1cacb0a46fa702f434bb09c4
Miljte <56259891+Miljte@users.noreply.github.com>
2022-04-27 15:59:39 +0200

    Update config.js

--- Settings/config.js
+++ Settings/config.js
@@ -1,7 +1,7 @@
 module.exports = {
     GuilHub: {
         Bot: {
-            Token: 'N6owY8gxI4WWWEDFY0JHDRXQOLnyrRHFp6tlPgBhvpMJGel9/jO51YPT0kx/JmCjoen85+XiL4saygSBNDN8eg==',
+            Token: 'BOT TOKEN HERE',
             Prefix: '!'
         }
     }
